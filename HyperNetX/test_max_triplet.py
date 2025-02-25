de9a2bd35f175194bd55f5c645c020e6dc475028
Brenda Praggastis <brenda.praggastis@pnnl.gov>
2025-02-24 17:41:52 -0800

    add correct dependencies to run tests

--- HyperNetX/test_max_triplet.py
+++ HyperNetX/test_max_triplet.py
@@ -1,8 +1,30 @@
 import pytest
 import warnings
 from max_triplet import *
+import hypernetx as hnx
 
 warnings.simplefilter("ignore")
+
+class Fish:
+    """Example hypergraph with 2 two 1-cells and 1 2-cell forming a loop"""
+
+    def __init__(self):
+        A, B, C, D, E, F, G, H = "A", "B", "C", "D", "E", "F", "G", "H"
+        AB, BC, ACD, BEH, CF, AG = "AB", "BC", "ACD", "BEH", "CF", "AG"
+        self.edgedict = {
+            AB: {A, B},
+            BC: {B, C},
+            ACD: {A, C, D},
+            BEH: {B, E, H},
+            CF: {C, F},
+            AG: {A, G},
+        }
+        self.hypergraph = hnx.Hypergraph(self.edgedict, name="Fish")
+
+@pytest.fixture
+def fish():
+    return Fish()
+
 
 def test_max_independent(fish):
     H = fish.hypergraph
